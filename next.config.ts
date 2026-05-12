a323db377c8a92582c039496d55761e152b5eb88
Jack Blundin <140537688+jhmblundin@users.noreply.github.com>
2026-05-12 11:31:46 -0700

    added rerouting pages for clawcomp.net domain alignment

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,21 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "cdn.creazilla.com", pathname: "/**" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/JFTMjG5vvp",
+        permanent: false,
+      },
+      {
+        source: "/talvy",
+        destination:
+          "https://talvy.com/groups/join/019e1d02-1d5a-7f33-b445-4c45b851c67a",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
